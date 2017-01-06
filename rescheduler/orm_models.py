dfcb26c0c90e6697e7415ca84f30a3d865d9e9b8
RyanKJ <ryankj42@gmail.com>
2017-01-06 13:05:24 -0600

    Add files via upload

--- rescheduler/orm_models.py
+++ rescheduler/orm_models.py
@@ -12,9 +12,19 @@ from sqlalchemy.orm import sessionmaker, relationship, backref
 Base = declarative_base()
 
 class DB_Schedule(Base):
+    """ORM representation of an employee schedule
+    
+    A schedule has a start and end datetime to represent its duration. Each
+    schedule is assigned to a department. There are optional booleans to 
+    hide the start and/or end time of a schedule in the view (This is for 
+    cases where the user wants to assign an employee to schedule with an
+    undetermined end time, say black Friday.) Schedules may or may not have 
+    an employee assigned to them. In the case there is an assigned employee,
+    the cost() method can be called to determine the amount of USD this
+    schedule costs.
+    """
     
     __tablename__ = 'schedules'
-        
         
     id = Column(Integer, primary_key=True)
     calendar_date = Column(Date)
@@ -33,7 +43,7 @@ class DB_Schedule(Base):
     
     def __init__(self, start_dt, end_dt, s_undetermined, e_undetermined, 
                  department):
-
+        """Initialize a Schedule ORM object."""
         self.start_datetime = start_dt
         self.end_datetime = end_dt
         self.department = department
@@ -41,37 +51,33 @@ class DB_Schedule(Base):
         self.e_undetermined_time = e_undetermined
         
         # Using the start and end datetime objects, create correspoding start
-        # and end time objects for comparison with time objects
+        # and end time objects for comparison with time objects for Queries
         self.start_time = datetime.time(start_dt.hour, start_dt.minute)
         self.end_time = datetime.time(end_dt.hour, end_dt.minute)
         # Create cal_date for calendar month and year comparisons and sch_date
-        # for date comparisons
+        # for Queries involving datetime.date() objects.
         self.calendar_date = datetime.date(start_dt.year, start_dt.month, 1)
         self.schedule_date = datetime.date(start_dt.year, start_dt.month,
                                            start_dt.day)
         
         
     def cost(self):
-        # Cost is a fairly straight forward function...in theory
-        # 1 - Get a floating number that represents the amount of hours
-        #     the employee is scheduled, so, if a schedule is from 10 am to
-        #     4:45 pm, then the employee will work 6.75 hours
-        # 2 - Fetch employee wage
-        #     Return hours worked times employee wage. Return 0 if no employee
-        #     is assigned. The only difficult part is converting a time delta.
+        """Calculate the cost of this schedule given assigned employee."""
         if self.employee_id == None:
             return 0
-            
         timedelta = self.end_datetime - self.start_datetime
         hours = timedelta.seconds / 3600
         return hours * self.employee.wage
-        
-    def get_time(self):
-        hour = star
-        
-    
+
+
     
 class Unavailable_Schedule(Base):
+    """ORM representation of a vacation period with start and end date.
+    
+    A vacation is represented as two datetime.dates that represent the start
+    and end dates of a vacation, and then the employee assigned to that given
+    Vacation.
+    """
 
     __tablename__ = 'unavailable'
     
@@ -84,19 +90,27 @@ class Unavailable_Schedule(Base):
     employee = relationship('Employees')
     
     def __init__(self, start_datetime, end_datetime, employee_id):
-        
+        """Initialize a Vacation ORM object."""
         self.start_datetime = start_datetime
         self.end_datetime = end_datetime
         self.employee_id = employee_id
         
-    # Returns a string formatted start month, start day, start year - end month, end day, end year
+
     def get_str_dates(self):
+        """Return a string formatted month, day, year of start/end dates."""
         start_str = self.start_datetime.strftime("%B %d, %Y")
         end_str = self.end_datetime.strftime("%B %d, %Y")
         return start_str + " - " + end_str
         
         
 class UnavailableTime(Base):
+    """ORM representation of a repeating unavailabile time period.
+    
+    An UnavailableTime or repeating unavailability is represented as two 
+    datetime.time that represent the start and end times, an integer
+    representing the day of the week, and an assigned employee for this 
+    repeating unavailability.
+    """
 
     WEEKDAY_TO_STR = {0: 'Mon', 1: 'Tu', 2: 'Wed', 3: 'Thu', 
                       4: 'Fri', 5: 'Sat', 6: 'Sun'}
@@ -112,19 +126,31 @@ class UnavailableTime(Base):
     employee = relationship('Employees')
     
     def __init__(self, start_time, end_time, weekday, employee_id):
-        
+        """Initialize a repeating unavailability ORM object."""
         self.start_time = start_time
         self.end_time = end_time
         self.weekday = weekday
         self.employee_id = employee_id
         
-    # Returns a string formatted start month, start day, start year - end month, end day, end year
+    
     def get_str(self):
+        """Returns a string formatted weekday, start time - end time."""
         start_str = self.start_time.strftime("%I:%M %p")
         end_str = self.end_time.strftime("%I:%M %p")
         return self.WEEKDAY_TO_STR[self.weekday] + " " + start_str + " - " + end_str
         
+        
+        
 class Employees(Base):
+    """ORM representation of an employee.
+    
+    An employee is primarily represented by their employee_id which must be
+    a unique identifier. Then extra information such as their first and last
+    names, wage, departments they can work, desired hours, overtime, medical
+    benefit cost per month, workmans comp and social security. Then there
+    are foreign keys for schedules, vacations, and repeating unavailability
+    an employee can be assigned to.
+    """
 
     __tablename__ = 'Employees'
     
@@ -143,16 +169,15 @@ class Employees(Base):
     workmans_comp = Column(Integer)
     social_security = Column(Integer)
     
-    # These relationships are how we calculate the validity of assignment 
-    #to a schedule, total scheduled hours, total cost of employment, etc.
     schedules = relationship("DB_Schedule")
     unavailable_schedules = relationship("Unavailable_Schedule")
     unav_time_schedules = relationship("UnavailableTime")
     
+    
     def __init__(self, employee_id, first_name, last_name, p_department, 
                  alt1_department, alt2_department, wage, desired_hours, overtime,
                  medical, work_comp, soc_sec):
-    
+        """Initialize an Employee ORM object."""
         self.employee_id = employee_id
         self.first_name = first_name
         self.last_name = last_name
@@ -170,14 +195,19 @@ class Employees(Base):
         self.schedule = relationship("DB_Schedule")
         self.unavailable_schedule = relationship("Unavailable_Schedule")
         
+        
     def add_schedule(self, db_schedule):
+        """Add schedule to list of schedule's this employee is assigned to."""
         self.schedules.append(db_schedule)
     
     def remove_schedule(self, db_schedule):
+        """Remove schedule from list of assigned schedules for this employee."""
         index = self.schedules.index(db_schedule)
-        del self.schedules[index] # Can this be made more succinct?
+        del self.schedules[index]
+    
     
     def add_unavailable_schedule(self, unavailable_schedule):
+        
         self.unavailable_schedules.append(unavailable_schedule)
         
     def add_unav_time(self, unav_time):
@@ -223,7 +253,11 @@ class Employees(Base):
     def calculate_cost(self):
         pass
         
-class MonthSales(Base):  
+class MonthSales(Base):
+    """ORM representation of the total revenue for given month and year.
+    
+    
+    """
 
     __tablename__ = 'Sales'
     id = Column(Integer, primary_key=True)
@@ -231,7 +265,7 @@ class MonthSales(Base):
     total_sales = Column(Integer)
     
     def __init__(self, m_and_y, sales):
-    
+        """Initialize a MonthSales ORM object."""
         self.month_and_year = m_and_y
         self.total_sales = sales
         
@@ -244,24 +278,28 @@ class MonthSales(Base):
         
         
 class Department(Base):  
-
+    """ORM representation of a department.
+    
+    
+    """
+    
 
     __tablename__ = 'Departments'
     id = Column(Integer, primary_key=True)
     name = Column(String)
     
     def __init__(self, department):
+        """Initialize a Department ORM object."""
         self.name = department
 
         
 def start_db(db_name, test=False):
+    """Function to start database, for normal usage or for testing."""
     db = 'sqlite:///' + db_name + '.db'
     if test:
         db = 'sqlite:///' + db_name + 'test.db'
-
     engine = create_engine(db, echo=False)
     Base.metadata.create_all(engine)
-    
     Session = sessionmaker(bind=engine)
     session = Session()
     
@@ -273,6 +311,5 @@ def start_db(db_name, test=False):
             dep = Department(d)
             session.add(dep)
         session.commit()
-
         
     return session
