677c51289ec3eff2640b667b415ef244a7ea2ef5
RyanKJ <ryankj42@gmail.com>
2017-01-06 12:12:38 -0600

    Add files via upload

--- tests/test.py
+++ tests/test.py
@@ -350,7 +350,9 @@ class GetEligablesTest(unittest.TestCase):
     secondary, thus should never appear on the list of eligables.
     """
     
-    EMPLOYEE_NAMES = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I'}
+    EMPLOYEE_NAMES = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 
+                      4: 'E', 5: 'F', 6: 'G', 7: 'H', 
+                      8: 'I'}
     employees = []
     
     def setUp(self):
